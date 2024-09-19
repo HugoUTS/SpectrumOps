59bba818a388c4eab078c3dd48bc8c34bda0e89d
Kingshuk-MAC <13983919@student.uts.edu.au>
2024-09-19 16:27:04 +1000

    Added Weapon Pickup

---
+++ Assets/Scripts/Items/Weapon.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Weapon", menuName = "Items/Weapons")]
+public class Weapon : Item
+{
+    public GameObject prefab;
+    public int ammoCount;
+    public float range;
+    public WeaponType WeaponType;
+    public WeaponStyle weaponStyle;
+}
+
+public enum WeaponType { Melee, Pistol, AR, Shotgun, Sniper}
+public enum WeaponStyle { Primary, Secondary}
