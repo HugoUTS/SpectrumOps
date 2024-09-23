d97aa81b8c87694e713a66acb5d033bb44044995
Kyaw Thiha <alexplayer153@gmail.com>
2024-09-24 09:27:46 +1000

    Elevator now closes when the player enters inside

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    private bool startEndLevel = false;
+    public GameObject playerCamera;
+    public GameObject playerController;
+    public float lerpTime = 0;
+    public Animator elevatorAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startEndLevel == true)
+        {
+            playerCamera.transform.parent = null;
+            Destroy(playerController);
+            lerpTime += Time.deltaTime * 0.1f;
+            playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, transform.position, lerpTime);
+            playerCamera.transform.rotation = Quaternion.Lerp(playerCamera.transform.rotation, Quaternion.Euler(0, 0, 0), lerpTime);
+        }
+
+        if (lerpTime >= 0.1)
+        {
+            elevatorAnim.SetTrigger("EndStage");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            startEndLevel = true;
+        }
+    }
+}
