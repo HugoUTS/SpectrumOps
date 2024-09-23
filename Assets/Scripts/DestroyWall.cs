e366394b448235fd95014813eeb0688d49102359
Kyaw Thiha <alexplayer153@gmail.com>
2024-09-23 20:53:02 +1000

    Wall and other minor fixes

--- Assets/Scripts/DestroyWall.cs
+++ Assets/Scripts/DestroyWall.cs
@@ -4,39 +4,26 @@ using UnityEngine;
 
 public class DestroyWall : MonoBehaviour
 {
-    public string tagName = "Projectile"; // The tag to identify the bullet
-    public Material wallMaterial;
-    
+    public string tagName = "Projectile";
+    public GameObject destroyedWall;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        // Check if the colliding object is a bullet (Projectile)
         if (collision.gameObject.tag == tagName)
         {
-            // Get the Material of the bullet that hit the wall
-            Renderer bulletRenderer = collision.gameObject.GetComponent<Renderer>();
-
-            if (bulletRenderer != null)
-            {
-                Material bulletMaterial = bulletRenderer.material;
-
-                // Compare the material names instead of references to ensure they match
-                if (bulletMaterial.name == wallMaterial.name + " (Instance)" ||
-                    bulletMaterial.name == wallMaterial.name)
-                {
-                    // Instantiate the destroyed version of the wall and destroy the original wall
-                    if (collision.gameObject.tag == tagName)
-                    {
-                        
-                        Destroy(gameObject);
-                        Debug.Log("Wall destroyed by matching bullet color!");
-                    }
-                    else
-                    {
-                        Debug.Log("Wall not destroyed. Bullet color does not match.");
-                    }
-                }
-            }
+            Instantiate(destroyedWall, gameObject.transform.position, gameObject.transform.rotation);
+            Destroy(gameObject);
         }
     }
 }
