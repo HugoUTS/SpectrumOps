bf43bc9bfadcde1297dbb0f1328c04d9f04888e2
Kyaw Thiha <alexplayer153@gmail.com>
2024-10-03 19:48:29 +1000

    Revolver System WIP1

--- Assets/Scripts/Ammo/HUDController.cs
+++ Assets/Scripts/Ammo/HUDController.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDController : MonoBehaviour
 {
     // Assuming you have references to your HUD elements in the Unity Editor
     public GameObject[] ammoHUDSlots;  // Array of HUD slots for ammo
     public GameObject[] chamberHUDSlots;  // Array of HUD slots for chambers
+    public Color[] HUDColor; // For storing a list of colors for the revolver chamber and ammo box
 
     // Update the HUD for ammo boxes
     public void UpdateAmmoHUD(AmmoBox[] ammoBoxes, int selectedAmmoIndex)
@@ -22,16 +24,20 @@ public class HUDController : MonoBehaviour
             }
 
             // Highlight the currently selected ammo box
-            ammoHUDSlots[i].transform.localScale = (i == selectedAmmoIndex) ? Vector3.one * 1.2f : Vector3.one;
+            ammoHUDSlots[i].transform.localScale = (i == selectedAmmoIndex) ? Vector3.one : Vector3.one;
         }
     }
 
     // Update the HUD for the chambers (show which are loaded or empty)
-    public void UpdateChamberHUD(int[] chambers)
+    public void UpdateChamberHUD(int[] chambers, Color chColor)
     {
         for (int i = 0; i < chambers.Length; i++)
         {
             chamberHUDSlots[i].SetActive(chambers[i] == 1); // Show if loaded, hide if empty
+            if (chamberHUDSlots[i].activeSelf == true)
+            {
+                chamberHUDSlots[i].GetComponent<Image>().color = chColor;
+            }
         }
     }
 }
--- Assets/Scripts/Revolver/RevolverChamber.cs
+++ Assets/Scripts/Revolver/RevolverChamber.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RevolverChamber : MonoBehaviour
 {
+    public RectTransform chamberIcon; // Reference to gun chamber
     public int totalChambers = 6;    // Number of chambers in the revolver
     private int[] chambers;  // Store the bullet info for each chamber
     public int currentChamber = 0;   // The currently active chamber
+    public Color[] chamberColor; // Stored colors for bullet chamber
     public Transform bulletSpawnPoint;   // Where the bullet is fired from
     public AmmoManager ammoManager;    // Reference to the player's ammo manager
     public HUDController hudController;  // Reference to the HUD Controller for UI updates
@@ -15,7 +18,7 @@ public class RevolverChamber : MonoBehaviour
     private void Start()
     {
         chambers = new int[totalChambers];   // Initialize all chambers as empty (0 means empty, 1 means filled)
-        hudController.UpdateChamberHUD(chambers);
+        hudController.UpdateChamberHUD(chambers, chamberColor[0]);
     }
 
     // Load a bullet into the active chamber
@@ -24,11 +27,39 @@ public class RevolverChamber : MonoBehaviour
         if (chambers[currentChamber] == 0)
         {
             AmmoBox selectedAmmo = ammoManager.GetCurrentAmmoBox();  // Get selected ammo box
+            
             if (selectedAmmo != null)
             {
                 chambers[currentChamber] = 1;  // Mark the chamber as filled
+
+                switch (selectedAmmo.ammoColor) // Change color of chamber depending on the ammo box's color
+                {
+                    case "Red":
+                        {
+                            hudController.UpdateChamberHUD(chambers, chamberColor[1]);
+                        }
+                        break;
+
+                    case "Yellow":
+                        {
+                            hudController.UpdateChamberHUD(chambers, chamberColor[2]);
+                        }
+                        break;
+
+                    case "Green":
+                        {
+                            hudController.UpdateChamberHUD(chambers, chamberColor[3]);
+                        }
+                        break;
+
+                    case "Blue":
+                        {
+                            hudController.UpdateChamberHUD(chambers, chamberColor[4]);
+                        }
+                        break;
+                }
                 Debug.Log($"Loaded {selectedAmmo.ammoColor} bullet into chamber {currentChamber}.");
-                hudController.UpdateChamberHUD(chambers);
+                
             }
         }
         else
@@ -63,8 +94,8 @@ public class RevolverChamber : MonoBehaviour
                 Debug.Log($"Fired bullet from chamber {currentChamber}.");
 
                 // Rotate to the next chamber after firing
-                RotateChamber();
-                hudController.UpdateChamberHUD(chambers);  // Update HUD to reflect chamber status
+                RotateChamberRight();
+                hudController.UpdateChamberHUD(chambers, chamberColor[0]);  // Update HUD to reflect chamber status
             }
         }
         else
@@ -73,15 +104,34 @@ public class RevolverChamber : MonoBehaviour
         }
     }
 
-    // Rotate to the next chamber (anticlockwise)
-    public void RotateChamber()
+    public void RotateChamberLeft()
     {
-        currentChamber = (currentChamber + 1) % totalChambers;
+        currentChamber = (currentChamber - 1) % totalChambers;
+        chamberIcon.transform.Rotate(0, 0, 60);
         Debug.Log($"Revolver rotated to chamber {currentChamber}.");
     }
 
+    // Rotate to the next chamber (anticlockwise)
+    public void RotateChamberRight()
+    {
+        currentChamber = (currentChamber + 1) % totalChambers;
+        chamberIcon.transform.Rotate(0, 0, -60);
+        Debug.Log($"Revolver rotated to chamber {currentChamber}.");
+    }
+
+
     void Update()
     {
+        if(currentChamber > 5)
+        {
+            currentChamber = 0;
+        }
+
+        if (currentChamber < 0)
+        {
+            currentChamber = 5;
+        }
+
         // Fire the bullet when the player presses the left mouse button
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
@@ -92,6 +142,16 @@ public class RevolverChamber : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R))
         {
             LoadBullet();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            RotateChamberLeft();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            RotateChamberRight();
         }
     }
 }
