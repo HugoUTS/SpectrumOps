2ace065bafa7e0a84df25b76ed57f163724b7434
Kyaw Thiha <alexplayer153@gmail.com>
2024-10-14 15:07:54 +1100

    (Hotfix) Interact icon now appears again when the player looks at a Pickup

--- Assets/Scripts/Weapon/PlayerPickup.cs
+++ Assets/Scripts/Weapon/PlayerPickup.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class PlayerPickup : MonoBehaviour
 {
     public float pickupRange = 2f; // How close the player must be to pick up the item
+    public GameObject interactBox;
     public LayerMask pickupLayer; // The layer of objects that can be picked up (e.g., ammo boxes)
     public Transform playerCamera; // The player's camera for raycasting
     public AmmoManager ammoManager; // Reference to the AmmoManager for adding ammo boxes
 
     void Update()
     {
+        RaycastHit hit;
+        if (Physics.Raycast(playerCamera.position, playerCamera.forward, out hit, pickupRange, pickupLayer))
+        {
+            interactBox.SetActive(true);
+        }
+        else
+        {
+            interactBox.SetActive(false);
+        }
+
         // Use raycasting to detect items the player is aiming at within pickup range
         if (Input.GetKeyDown(KeyCode.E)) // Press 'E' to interact
         {
-            RaycastHit hit;
             if (Physics.Raycast(playerCamera.position, playerCamera.forward, out hit, pickupRange, pickupLayer))
             {
+
                 AmmoBoxPickup ammoBoxPickup = hit.collider.GetComponent<AmmoBoxPickup>();
                 if (ammoBoxPickup != null)
                 {
