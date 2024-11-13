e244c36457a2a15900e073ec581f84a5baa0805a
Kyaw Thiha <alexplayer153@gmail.com>
2024-11-13 11:23:54 +1100

    (Hotfix) Slowed down the lasers rotation speed in LaserSpeen script

--- Assets/LaserSpeen.cs
+++ Assets/LaserSpeen.cs
@@ -8,6 +8,6 @@ public class LaserSpeen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.Rotate(0, 0.2f, 0);
+        gameObject.transform.Rotate(0, 0.06f, 0);
     }
 }
